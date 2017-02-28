b160efc0ea0f27f64e506ef7efaa09ade072ffa1
Easwar Moorthy <easwarvem@gmail.com>
2017-02-28 21:31:10 +0530

    Procfile

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
